0ef7c451bbc8d2ce7e24e4fff20d7f36c1e91d79
Chanki Biswas <biswaschanki@gmail.com>
2022-01-04 16:15:08 +0530

    Inheritance example

---
+++ Python/Class/Manager.py
@@ -0,0 +1,11 @@
+from Class.Employee import Employee
+
+
+class Manager(Employee):
+    def __init__(self, age, name):
+        super().__init__(name)
+        print(self.x)
+
+
+m = Manager(30, "Mahesh")
+print(m.x)
